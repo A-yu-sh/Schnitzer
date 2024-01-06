4539e6237789efd41812177ec4f2cb457f0d950c
A-yu-sh <ayush.wd.dev@gmail.com>
2024-01-06 14:28:38 +0530

    Update: Changed The Function to get data by id

--- app/api/Operations/route.js
+++ app/api/Operations/route.js
@@ -1,9 +1,9 @@
 import { CONNECT_MONGO_DB } from "@/libs/ConnectMongoDB";
 import PRODUCT_MODEL from "@/model/ProductModel";
 import UserMODEL from "@/model/UserModel";
-import { Redis } from "ioredis";
+// import { Redis } from "ioredis";
 
-const CLI = new Redis(6379);
+// const CLI = new Redis(6379);
 
 export async function GET_TRENDING_DATA() {
   await CONNECT_MONGO_DB();
@@ -21,16 +21,16 @@ export async function GET_NEW_LAUNCH_DATA() {
 
 export async function GET_DATA_BY_ID(id) {
   await CONNECT_MONGO_DB();
-  const Cache_Product_By_Id = await CLI.get(`Key : ${id}`);
-  if (Cache_Product_By_Id) {
-    const Product = JSON.parse(Cache_Product_By_Id);
-    return Product;
-  } else {
-    const res = await PRODUCT_MODEL.findById(id);
-    await CLI.set(`Key : ${id}`, JSON.stringify(res));
-    CLI.expire(`Key : ${id}`, 600);
-    return res;
-  }
+  // const Cache_Product_By_Id = await CLI.get(`Key : ${id}`);
+  // if (Cache_Product_By_Id) {
+  // const Product = JSON.parse(Cache_Product_By_Id);
+  // return Product;
+  // } else {
+  const res = await PRODUCT_MODEL.findById(id);
+  // await CLI.set(`Key : ${id}`, JSON.stringify(res));
+  // CLI.expire(`Key : ${id}`, 600);
+  return res;
+  // }
 }
 
 export const GET_DATA_BY_CATEGORY = async (Category) => {
