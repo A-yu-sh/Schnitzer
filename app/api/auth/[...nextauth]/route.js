b995b55ee91f3895bd8e8badeed0dbff8a8d4562
A-yu-sh <ayush.wd.dev@gmail.com>
2024-01-06 22:19:02 +0530

    adding next auth secret

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -13,7 +13,7 @@ export const authOptions = {
     // ...add more providers here
   ],
 
-  secret: process.env.AUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
 
   callbacks: {
     async signIn({ user, account }) {
