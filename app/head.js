e7e955ec94f5a3f4c84308c5e136f4c68ef5b0e7
A-yu-sh <ayush.wd.dev@gmail.com>
2024-01-06 17:39:03 +0530

    Added Head

---
+++ app/head.js
@@ -0,0 +1,14 @@
+export default function Head() {
+  return (
+    <>
+      <title>Schnitzer | Explore The Innovation</title>
+      <meta
+        name="description"
+        title="Explore Cutting-Edge Gadgets at Schnitzer Tech Hub | Earphones, Smartwatches, Speakers & More"
+        content="Discover the future of tech at Schnitzer Tech Hub. Elevate your lifestyle with our premium collection of earphones, smartwatches, speakers, and headphones. Immerse yourself in the world of SmartWatch Technology. Shop now for innovation and style!"
+      />
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/favicon.png" />
+    </>
+  );
+}
