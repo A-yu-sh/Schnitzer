eda9b7267a3d9e0fe45470d9765310f4e321385f
A-yu-sh <ayush.wd.dev@gmail.com>
2024-01-17 21:05:10 +0530

    changed card design

--- app/collection/SortingButtons.js
+++ app/collection/SortingButtons.js
@@ -4,10 +4,10 @@ import { useDebouncedCallback } from "use-debounce";
 import { useSearchParams, useRouter, usePathname } from "next/navigation";
 
 const label = [
-  { label: "Sort By", slug: "Sort By", disabled: true },
-  { label: "Relevance", slug: "Relevance", disabled: false },
-  { label: "Price: Low to High", slug: "Ascending", disabled: false },
-  { label: "Price: High to Low", slug: "Descending", disabled: false },
+  { label: "Sort By", slug: "Sort By", Checkdisabled: true },
+  { label: "Relevance", slug: "Relevance", Checkdisabled: false },
+  { label: "Price: Low to High", slug: "Ascending", Checkdisabled: false },
+  { label: "Price: High to Low", slug: "Descending", Checkdisabled: false },
 ];
 
 const SortingButtons = () => {
@@ -27,15 +27,14 @@ const SortingButtons = () => {
 
   return (
     <div>
-      <div className="flex justify-center mt-5 md:justify-end">
+      <div className="flex justify-end mt-5 md:justify-end">
         <label className="flex  text-sm font-medium text-gray-900 dark:text-white">
-          {/* <span className="mt-2 mr-2 text-sm md:text-lg">Sort By </span> */}
           <select
             onChange={(e) => UpdateQueryParameter(e.target.value)}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-yellow-500 focus:border-yellow-500 block w-1/7 mr-5 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-yellow-500 dark:focus:border-yellow-500">
             {label.map((e) => {
               return (
-                <option key={e.slug} value={e.slug} disabled={e.disabled}>
+                <option key={e.slug} value={e.slug} disabled={e.Checkdisabled}>
                   {e.label}
                 </option>
               );
--- app/components/Cards.js
+++ app/components/Cards.js
@@ -23,52 +23,53 @@ let value = [
 
 const Cards = ({ id, name, img, Price, Category, description, catchline }) => {
   return (
-    <Container>
-      <div className="border-2  w-[17rem] md:w-[20rem] rounded-lg p-5 mt-10">
-        <Link
-          href="/collection/[category]/[id]"
-          as={`/collection/${Category}/${id}`}>
-          <div className=" ">
-            <div className="flex justify-center">
-              <Image
-                src={img}
-                width={1050}
-                height={1050}
-                priority={true}
-                alt={`image of ${name}`}
-                className="flex justify-center bg-stone-100 rounded-t-lg   h-[15rem] w-[18rem] p-20"
-              />
-            </div>
-
-            <p
-              className={`${MONTSERRAT_price.className} flex justify-center rounded-b-lg py-2 font-bold bg-yellow-300`}>
-              {"AMOLED Display" || "60hrs playback" || "ENXM Technology"}
-            </p>
-
-            <p className={`${MONTSERRAT.className} font-bold text-xl mt-2  `}>
-              {name}
-            </p>
-            <p
-              className={`${MONTSERRAT_price.className} flex justify-between font-bold text-2xl mt-2  `}>
-              ${Price}
-            </p>
-
-            <hr />
-            <div className="grid grid-cols-2 md:grid-cols-2 gap-5 mt-4">
-              {value.map((e) => {
-                return (
-                  <div
-                    key={e.label}
-                    className="text-[10px]  bg-blue-50 flex justify-center p-1 rounded-lg ">
-                    {e.label}
-                  </div>
-                );
-              })}
-            </div>
+    // <Container>
+    <div className="border-2  w-[37rem] md:w-[20rem] rounded-lg p-5 mt-10">
+      <Link
+        href="/collection/[category]/[id]"
+        as={`/collection/${Category}/${id}`}>
+        <div className=" ">
+          <div className="flex justify-center">
+            <Image
+              src={img}
+              width={1050}
+              height={1050}
+              priority={true}
+              alt={`image of ${name}`}
+              className="flex justify-center bg-stone-100 rounded-t-lg md:h-[15rem] w-full md:w-[18rem] p-20"
+            />
           </div>
-        </Link>
-      </div>
-    </Container>
+
+          <p
+            className={`${MONTSERRAT_price.className} flex justify-center rounded-b-lg py-2 font-bold bg-yellow-300`}>
+            {"AMOLED Display" || "60hrs playback" || "ENXM Technology"}
+          </p>
+
+          <p
+            className={`${MONTSERRAT.className} font-bold text-xl mt-7 md:mt-2  `}>
+            {name}
+          </p>
+          <p
+            className={`${MONTSERRAT_price.className} flex justify-between font-bold text-2xl mt-5 md:mt-2  `}>
+            ${Price}
+          </p>
+
+          <hr />
+          <div className="grid grid-cols-2 md:grid-cols-2 gap-5 mt-4">
+            {value.map((e) => {
+              return (
+                <div
+                  key={e.label}
+                  className="text-[10px]  bg-blue-50 flex justify-center p-1 rounded-lg ">
+                  {e.label}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </Link>
+    </div>
+    // </Container>
   );
 };
 
